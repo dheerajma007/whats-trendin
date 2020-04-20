d39c77edde06c56caae0c3c689b232d996d37df8
unknown <dheerajma007@gmail.com>
2020-04-20 18:29:20 +0530

    fixed npm start issue

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const path = require('path');
 const app = express();
 
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('/welcome', (req, res) => res.send('This is a placeholder text'));
 
 // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'../client/build/index.html'));
+    res.sendFile(path.join(__dirname+'client/build/index.html'));
 });
 
 app.listen(8080);
