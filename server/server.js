50a6aaa7248d7301ccef80168545a61f2e851306
unknown <dheerajma007@gmail.com>
2020-04-20 16:54:06 +0530

    updated setup

--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get('/welcome', (req, res) => res.send('This is a placeholder text'))
 
