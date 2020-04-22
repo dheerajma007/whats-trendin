62c40dada900006a3438bee333a080ec48be9780
unknown <dheerajma007@gmail.com>
2020-04-22 22:34:40 +0530

    fixed test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/Welcome to whats-trendin!/i);
+  const linkElement = getByText(/Whats trendin?/i);
   expect(linkElement).toBeInTheDocument();
 });
